6a9bd608da2f128d18b4d0220d2823b1623d55b5
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2024-01-14 14:44:45 +0700

    Remove SW Offline

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,15 @@
 <template>
-  <template v-if="offlineReady || needRefresh">
-    <div class="h-[100vh] bg-background flex flex-col gap-2 justify-center items-center">
-      <div class="mt-1">
-        <span v-if="offlineReady"> App ready to work offline </span>
-        <span v-else>New content available, click on reload button to update.</span>
-      </div>
-
-      <div class="flex items-center justify-center gap-2">
-        <ElButton
-          type="primary"
-          @click="() => updateServiceWorker()"
-        >
-          Reload
-        </ElButton>
-
-        <ElButton
-          type="primary"
-          @click="onClose"
-        >
-          Close
-        </ElButton>
-      </div>
-    </div>
-  </template>
-
-  <template v-else>
-    <RouterView v-slot="{ Component }">
-      <Component :is="Component" />
-    </RouterView>
-  </template>
+  <RouterView v-slot="{ Component }">
+    <Component :is="Component" />
+  </RouterView>
 </template>
 
 <script setup lang="ts">
-import { useRegisterSW } from "virtual:pwa-register/vue";
 import { firebaseCloudMessaging } from '@fb'
 
-const { offlineReady, needRefresh, updateServiceWorker } = useRegisterSW();
 const { getToken } = firebaseCloudMessaging()
 
-const onClose = async () => {
-  offlineReady.value = false;
-  needRefresh.value = false;
-};
 
 
-onMounted(async() => console.log('token : ', await getToken))
+onMounted(async() => console.log('fcm token : ', await getToken))
 </script>
