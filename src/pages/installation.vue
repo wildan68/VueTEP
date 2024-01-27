cf3daf5b1b0b57594b7437968600a51de186262e
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2024-01-27 18:15:18 +0700

    Fixed Installation Page

--- src/pages/installation.vue
+++ src/pages/installation.vue
@@ -1,55 +1,72 @@
 <template>
-  <div class="flex flex-col gap-6">
-    <ContentCard>
-      <template #title>
-        <span class="text-xl font-extrabold cursor-pointer text-warning">#</span> Installation
-      </template>
-      <template #subtitle>
-        <ul class="mt-4 ml-6 list-disc">
-          <li class="mb-4">
-            <div class="flex flex-col gap-4">
-              Clone this repository or download the zip file
+  <ContentCard transparent>
+    <template #title>
+      <span class="text-xl font-extrabold cursor-pointer text-warning">#</span> Installation
+    </template>
+    <template #subtitle>
+      Install VueTEP to starter your project
+    </template>
+  </ContentCard>
 
-              <div class="flex items-center gap-2">
-                <div
-                  class="p-2.5 border rounded-md bg-zinc-100 dark:bg-zinc-900 h-fit dark:border-zinc-500 cursor-pointer hover:text-zinc-600" 
-                  @click="copyRepository"
-                >
-                  <IconTablerCopy />
-                </div>
-  
-                <div
-                  v-highlightjs
-                  class="h-fit"
-                >
-                  <code class="border rounded-md bg-zinc-100 dark:bg-zinc-900 javascript dark:border-zinc-500">{{ repositoryLink }}</code>
-                </div>
-  
-                <div
-                  class="p-2.5 border rounded-md bg-zinc-100 dark:bg-zinc-900 h-fit dark:border-zinc-500 cursor-pointer hover:text-zinc-600"
-                  @click="openRepository"
-                >
-                  <IconTablerBrandGithub />
-                </div>
-              </div>
-            </div>
-          </li>
-          <li class="mb-4">
-            Remove Git
-          </li>
-          <li class="mb-4">
-            Install the package using <TextHighlight>npm</TextHighlight> or <TextHighlight>yarn</TextHighlight>
-          </li>
-          <li class="mb-4">
-            Import the package in your <TextHighlight>main.ts</TextHighlight> file
-          </li>
-          <li class="mb-4">
-            Use the component in your template
-          </li>
-        </ul>
-      </template>
-    </ContentCard>
-  </div>
+  <ContentCard>
+    <div class="flex flex-col gap-4">
+      <div class="">
+        <ElButton
+          plain
+          round
+          @click="openRepository"
+        >
+          <template #icon>
+            <IconTablerBrandGithub />
+          </template>
+
+          <span class="font-semibold">VueTEP Github</span>
+        </ElButton>
+      </div>
+    
+      <div class="flex gap-2">
+        <span class="text-sm">1.</span> 
+        <a
+          href="https://github.com/wildan68/VueTEP/releases"
+          target="_blank"
+          class="text-sm text-primary"
+          rel="noopener noreferrer"
+        >
+          Open Github Release
+        </a>
+      </div>
+
+      <div class="flex gap-2">
+        <span class="text-sm">2.</span> 
+        <span class="text-sm">Download the latest release</span>
+      </div>
+
+      <div class="flex gap-2">
+        <span class="text-sm">3.</span> 
+        <span class="text-sm">Extract the zip file</span>
+      </div>
+
+      <div class="flex gap-2">
+        <span class="text-sm">4.</span> 
+        <span class="text-sm">Open the extracted folder</span>
+      </div>
+
+      <div class="flex gap-2">
+        <span class="text-sm">5.</span> 
+        <span class="text-sm">Open your terminal</span>
+      </div>
+
+      <div class="flex gap-2">
+        <span class="text-sm">6.</span> 
+        <span class="text-sm">Run <TextHighlight>npm install</TextHighlight></span>
+      </div>
+
+      <div class="flex gap-2">
+        <span class="text-sm">7.</span> 
+        <span class="text-sm">Run <TextHighlight>npm run dev</TextHighlight> for development</span>
+      </div>
+    </div>
+  </ContentCard>
 </template>
   
 <script setup lang="ts">  
